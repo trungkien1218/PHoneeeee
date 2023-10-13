fea7f743bbe8816c0f6ba884149d635763116e27
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-10-13 17:17:06 +0700

    css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1 style={{textAlign:'center', color:'rgb(188, 254, 253)'}}>Thế giới công nghệ Lam </h1>
+    <>
+
+      <section className='sec1'>
+        <div className='BG-main'>
+          <h1
+            className='myshadow '
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              paddingTop: '550px',
+              fontSize: '70px'
+            }}>Technology world Lam </h1>
+          <p className='myshadow ' style={{
+            textAlign: 'center',
+            color: 'white',
+            paddingTop: '20px',
+            fontSize: '25px'
+          }}>Always accompany you wherever you are</p>
+        </div>
+      </section>
+      <section className='sec2 container'>
+        <div className='text2 '>
+          <span className='myshadow2 ' style={{fontSize:'40px', color: '#01455a'}}>Information technology</span> (English: Information Technology - IT), IT abbreviation, is a branch of engineering using computer and computer software for conversion, storage, protection, processing, transmission and collection Cross information.
+
+        </div>
+        <div className="img2">
+
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/routes/ShoppingCart.jsx
+++ src/routes/ShoppingCart.jsx
@@ -11,7 +11,7 @@ const ShoppingCart = () => {
   const { items, totalPrice } = useShoppingCartContext();
 
   const { onIncreItem, onDecreItem } = useShoppingCartContext();
- 
+
   const handleIncreItem = (product) => {
     console.log(product)
     onIncreItem({ productId: product.id, quantity: + 1 })
@@ -25,56 +25,51 @@ const ShoppingCart = () => {
     onTotalPrice({ productId : product.id , totalPrice : 0})
   }*/}
 
-/// const totalP =useSelector(totalPrice);
+  /// const totalP =useSelector(totalPrice);
 
 
 
   return (
     <>
-      <main className='main-cart'>
-        <h1>ShoppingCart page</h1>
-        <div>Cart list</div>
-        <div className='table-cart container'>
+      <main className='main-cart '>
+        <h1 className='ShoppingCart-page'>ShoppingCart page</h1>
+        <div className='list'>Cart list</div>
+        <div className='table-cart container scroll'>
           {items.map(product => (
             <div key={product.product.id} to={`/items/${product.product.id}`}>
-              <div className='flex'>
-                <div className='cart-prd'>
-                  <div className='cart-img'>
-                    <img
-                      src={product.product.thumbnail}
-                      alt={product.product.title}
-                      className='cart-img-prd'
-                    />
-                  </div>
-                  <div>
-                    <h1 className='cart-name'>
-                      {product.product.title}
-                    </h1>
-                    <div className='cart-price'>
-                      Price:{product.product.price}
+              <div className='list-cart'>
+                <div className='flex'>
+                  <div className='cart-prd'>
+                    <div className='cart-img'>
+                      <img
+                        src={product.product.thumbnail}
+                        alt={product.product.title}
+                        className='cart-img-prd'
+                      />
+                    </div>
+                    <div>
+                      <h1 className='cart-name'>
+                        {product.product.title}
+                      </h1>
+                      <div className='cart-price'>
+                        Price:    ${product.product.price}
+                      </div>
                     </div>
                   </div>
+                  <div>
+                    <Button onClick={() => handleDecreItem(product.product)}> - </Button>
+                    <span style={{ padding: '10px' }}>{product.quantity}</span>
+                    <Button onClick={() => handleIncreItem(product.product)}>+</Button>
+                  </div>
                 </div>
-                <div>
-                  <Button
-                    /// onPrice={() => handleTotalPrice(product.price)}
-                    onClick={() => handleDecreItem(product.product)}>
-                    -
-                  </Button>
-                  <span style={{ padding: '10px' }}>{product.quantity}</span>
-                  <Button
-                    /// onPrice={() => handleTotalPrice(product.price)}
-                    onClick={() => handleIncreItem(product.product)}>+</Button>
-                </div>
-              </div>
-              <div className='total-price'>
-                <p>Tổng tiền:</p>
-                <div>{totalPrice}</div>
               </div>
             </div>
           ))}
-
         </div>
+        <div className='total-price'>
+                <p style={{fontSize:'30px', fontWeight:'600'}}>Tổng tiền:</p>
+                <div style={{fontSize:'30px'}}>${totalPrice}</div>
+              </div>
       </main>
     </>
   )
